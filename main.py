c2219d6edf23a5e6f872b4eabcb6d45c41735f26
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-06 16:07:26 -0300

    feat: class employee done

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from management.employee import Employee
 def init():
     funcionario = Employee('alexandre araujo', '12345678')
     print(funcionario.__dict__)
+    print(funcionario)
 
 if __name__ == "__main__":
     init()
--- management/employee.py
+++ management/employee.py
@@ -4,9 +4,9 @@ class Employee:
     function = 'Employee'
 
     def __init__(self, full_name, cpf, wage = 3000, admission = dt.now()):
-        self.full_name = full_name.title(),
-        self.cpf = cpf,
-        self.wage = wage,
+        self.full_name = full_name.title()
+        self.cpf = cpf
+        self.wage = wage
         self.admission = admission.strftime('%d-%m-%Y')
 
     def __str__(self):
